f3dee254aa938ce8bb23687c0fca559dc20fd248
cats1040 <vshlvani1234@gmail.com>
2025-11-15 17:51:21 +0530

    Database json-server

--- nextJS/meow-app/src/app/page.js
+++ nextJS/meow-app/src/app/page.js
@@ -25,41 +25,41 @@ export default async function Home() {
   console.log("------------Fetched products from server ", products);
 
   // const products = [
-  //   {
-  //     id: "p1",
-  //     name: "Product 1",
-  //     price: 10,
-  //     description: "This is product 1",
-  //     image: "https://placehold.co/300x200",
-  //   },
-  //   {
-  //     id: "p2",
-  //     name: "Product 2",
-  //     price: 20,
-  //     image: "https://placehold.co/300x200",
-  //     description: "This is product 2",
-  //   },
-  //   {
-  //     id: "p3",
-  //     name: "Product 3",
-  //     price: 30,
-  //     image: "https://placehold.co/300x200",
-  //     description: "This is product 3",
-  //   },
-  //   {
-  //     id: "p4",
-  //     name: "Product 4",
-  //     price: 40,
-  //     image: "https://placehold.co/300x200",
-  //     description: "This is product 4",
-  //   },
-  //   {
-  //     id: "p5",
-  //     name: "Product 5",
-  //     price: 50,
-  //     image: "https://placehold.co/300x200",
-  //     description: "This is product 5",
-  //   },
+  // {
+  //   id: "p1",
+  //   name: "Product 1",
+  //   price: 10,
+  //   description: "This is product 1",
+  //   image: "https://placehold.co/300x200",
+  // },
+  // {
+  //   id: "p2",
+  //   name: "Product 2",
+  //   price: 20,
+  //   image: "https://placehold.co/300x200",
+  //   description: "This is product 2",
+  // },
+  // {
+  //   id: "p3",
+  //   name: "Product 3",
+  //   price: 30,
+  //   image: "https://placehold.co/300x200",
+  //   description: "This is product 3",
+  // },
+  // {
+  //   id: "p4",
+  //   name: "Product 4",
+  //   price: 40,
+  //   image: "https://placehold.co/300x200",
+  //   description: "This is product 4",
+  // },
+  // {
+  //   id: "p5",
+  //   name: "Product 5",
+  //   price: 50,
+  //   image: "https://placehold.co/300x200",
+  //   description: "This is product 5",
+  // },
   // ];
 
   return (
