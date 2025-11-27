4e403b62d86c6c4ac4e5e818fbee3ab444148041
cats1040 <vshlvani1234@gmail.com>
2025-11-27 22:14:23 +0530

    Import statements and router

--- class-project/backend/index.js
+++ class-project/backend/index.js
@@ -1,18 +1,17 @@
-var express = require("express");
+// var express = require("express");
+import express from "express";
+import bodyParser from "body-parser";
+import productsRoutes from "./routes/products.js";
+
 var app = express();
-var fileSystem = require("fs");
-
-// var data = require("./db.json"); // bad way for db
-
 var port = 5000;
 
-var bodyParser = require("body-parser");
-const { stringify } = require("querystring");
 app.use(bodyParser.json()); // to process incoming post requests to json (high level)
 
+// /*splat - matches all paths other than root path
 app.use("/*splat", (req, res, next) => {
   // Allow CORS
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, PUT, DELETE, OPTIONS"
@@ -21,153 +20,10 @@ app.use("/*splat", (req, res, next) => {
   next();
 });
 
+app.use("/products", productsRoutes);
+
 app.get("/", (req, res) => {
   res.end("Hello world\n");
-});
-
-app.get("/products", (req, res) => {
-  fileSystem.readFile("./db.json", "utf8", (err, data) => {
-    if (err) {
-      console.error("Error reading db.json file:", err);
-      res.status(500).json({ error: "Internal Server Error" });
-      return;
-    }
-    const currentData = JSON.parse(data);
-    const products = currentData.products;
-    console.log(products);
-    res.json(products);
-  });
-});
-
-app.post("/products", (req, res) => {
-  const newProduct = req.body;
-
-  if (newProduct && newProduct.name && newProduct.price) {
-    console.log("Received new product: ", newProduct);
-  } else {
-    res.status(400).json({ error: "Invalid product data" });
-  }
-
-  fileSystem.readFile("./db.json", "utf8", (err, data) => {
-    if (err) {
-      console.error("Error reading db.json file:", err);
-      res.status(500).json({ error: "Internal Server Error" });
-      return;
-    }
-
-    const currentData = JSON.parse(data);
-    currentData.products.push(newProduct);
-
-    fileSystem.writeFile(
-      "./db.json",
-      JSON.stringify(currentData),
-      (err, data) => {
-        if (err) {
-          console.error("Error writing to db.json file:", err);
-          res.status(500).json({ error: "Internal Server Error" });
-          return;
-        }
-
-        console.log("db.json file updated successfully");
-        res.status(201).json({ message: "Product created successfully" });
-        return;
-      }
-    );
-  });
-});
-
-app.put("/products/:id", (req, res) => {
-  const { id } = req.params;
-  const updatedData = req.body;
-
-  fileSystem.readFile("./db.json", "utf8", (err, data) => {
-    if (err) {
-      console.error("Error reading db.json", err);
-      return res.status(500).json({ error: "Internal Server Error" });
-    }
-
-    const currentData = JSON.parse(data);
-    const products = currentData.products;
-
-    // find product index
-    const index = products.findIndex((p) => p.id == id);
-
-    if (index === -1) {
-      return res.status(404).json({ error: "Product not found" });
-    }
-
-    // update product
-    // data not sanitized
-    products[index] = { ...products[index], ...updatedData };
-
-    // save back to file
-    fileSystem.writeFile(
-      "./db.json",
-      JSON.stringify(currentData, null, 2),
-      (err) => {
-        if (err) {
-          console.error("Error writing db.json");
-          return res.status(500).json({ error: "Internal Server Error" });
-        }
-
-        return res.json({
-          message: "Product updated successfully",
-          updatedProduct: products[index],
-        });
-      }
-    );
-  });
-});
-
-// Sample Input: localhost:5000/products?id=3
-// app.put("/products", (req, res) => {
-//   // Update product by ID
-//   let queryParams = req.query;
-//   let body = req.body;
-
-//   console.log("Params:", queryParams);
-//   console.log("Body:", body);
-// });
-
-app.delete("/products/:id", (req, res) => {
-  const { id } = req.params;
-
-  fileSystem.readFile("./db.json", "utf8", (err, data) => {
-    if (err) {
-      console.error("Error reading db.json", err);
-      return res.status(500).json({ error: "Internal Server Error" });
-    }
-
-    const currentData = JSON.parse(data);
-    const products = currentData.products;
-
-    // find product index
-    const index = products.findIndex((p) => p.id == id);
-
-    if (index == -1) {
-      return res.status(404).json({ error: "Product not found" });
-    }
-
-    // update products
-    products.splice(index, 1);
-
-    // save back to file
-    fileSystem.writeFile(
-      "./db.json",
-      JSON.stringify(currentData, null, 2),
-      (err) => {
-        if (err) {
-          console.error("Error writing db.json");
-          return res.status(500).json({ error: "Internal Server Error" });
-        }
-
-        return res.json({
-          message: "Product removed successfully",
-          removedProduct: products[index],
-        });
-      }
-    );
-  });
 });
 
 app.listen(port, () => {
---
+++ class-project/backend/routes/products.js
@@ -0,0 +1,142 @@
+import express, { Router } from "express";
+
+const router = express.Router();
+
+import fileSystem from "fs";
+
+router.get("/", (req, res) => {
+  fileSystem.readFile("./db.json", "utf8", (err, data) => {
+    if (err) {
+      console.error("Error reading db.json file:", err);
+      res.status(500).json({ error: "Internal Server Error" });
+      return;
+    }
+    const currentData = JSON.parse(data);
+    const products = currentData.products;
+    console.log(products);
+    res.json(products);
+  });
+});
+
+router.post("/", (req, res) => {
+  const newProduct = req.body;
+
+  if (newProduct && newProduct.name && newProduct.price) {
+    console.log("Received new product: ", newProduct);
+  } else {
+    res.status(400).json({ error: "Invalid product data" });
+  }
+
+  fileSystem.readFile("./db.json", "utf8", (err, data) => {
+    if (err) {
+      console.error("Error reading db.json file:", err);
+      res.status(500).json({ error: "Internal Server Error" });
+      return;
+    }
+
+    const currentData = JSON.parse(data);
+    currentData.products.push(newProduct);
+
+    fileSystem.writeFile(
+      "./db.json",
+      JSON.stringify(currentData),
+      (err, data) => {
+        if (err) {
+          console.error("Error writing to db.json file:", err);
+          res.status(500).json({ error: "Internal Server Error" });
+          return;
+        }
+
+        console.log("db.json file updated successfully");
+        res.status(201).json({ message: "Product created successfully" });
+        return;
+      }
+    );
+  });
+});
+
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const updatedData = req.body;
+
+  fileSystem.readFile("./db.json", "utf8", (err, data) => {
+    if (err) {
+      console.error("Error reading db.json", err);
+      return res.status(500).json({ error: "Internal Server Error" });
+    }
+
+    const currentData = JSON.parse(data);
+    const products = currentData.products;
+
+    // find product index
+    const index = products.findIndex((p) => p.id == id);
+
+    if (index === -1) {
+      return res.status(404).json({ error: "Product not found" });
+    }
+
+    // update product
+    // data not sanitized
+    products[index] = { ...products[index], ...updatedData };
+
+    // save back to file
+    fileSystem.writeFile(
+      "./db.json",
+      JSON.stringify(currentData, null, 2),
+      (err) => {
+        if (err) {
+          console.error("Error writing db.json");
+          return res.status(500).json({ error: "Internal Server Error" });
+        }
+
+        return res.json({
+          message: "Product updated successfully",
+          updatedProduct: products[index],
+        });
+      }
+    );
+  });
+});
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+
+  fileSystem.readFile("./db.json", "utf8", (err, data) => {
+    if (err) {
+      console.error("Error reading db.json", err);
+      return res.status(500).json({ error: "Internal Server Error" });
+    }
+
+    const currentData = JSON.parse(data);
+    const products = currentData.products;
+
+    // find product index
+    const index = products.findIndex((p) => p.id == id);
+
+    if (index == -1) {
+      return res.status(404).json({ error: "Product not found" });
+    }
+
+    // update products
+    products.splice(index, 1);
+
+    // save back to file
+    fileSystem.writeFile(
+      "./db.json",
+      JSON.stringify(currentData, null, 2),
+      (err) => {
+        if (err) {
+          console.error("Error writing db.json");
+          return res.status(500).json({ error: "Internal Server Error" });
+        }
+
+        return res.json({
+          message: "Product removed successfully",
+          removedProduct: products[index],
+        });
+      }
+    );
+  });
+});
+
+export default router;
